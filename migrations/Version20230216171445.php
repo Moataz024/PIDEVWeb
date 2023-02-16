13edbab771f2d9dc7b0a6d704d4f221a94d458c1
louayth <thamilouay@gmail.com>
2023-02-16 18:23:20 +0100

    loup

--- migrations/Version20230216171445.php
+++ migrations/Version20230216171445.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230216164504 extends AbstractMigration
+final class Version20230216171445 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,9 +22,14 @@ final class Version20230216164504 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, organisateur_id INT DEFAULT NULL, nom VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, INDEX IDX_3BAE0AA7D936B2FA (organisateur_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE event_user (event_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_92589AE271F7E88B (event_id), INDEX IDX_92589AE2A76ED395 (user_id), PRIMARY KEY(event_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sponsor_e (id INT AUTO_INCREMENT NOT NULL, nom_sponsor VARCHAR(255) NOT NULL, email_sponsor VARCHAR(255) NOT NULL, tel_sponsor VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sponsor_e_event (sponsor_e_id INT NOT NULL, event_id INT NOT NULL, INDEX IDX_ED8F5E6541E87EF (sponsor_e_id), INDEX IDX_ED8F5E671F7E88B (event_id), PRIMARY KEY(sponsor_e_id, event_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA7D936B2FA FOREIGN KEY (organisateur_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE event_user ADD CONSTRAINT FK_92589AE271F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE event_user ADD CONSTRAINT FK_92589AE2A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE sponsor_e_event ADD CONSTRAINT FK_ED8F5E6541E87EF FOREIGN KEY (sponsor_e_id) REFERENCES sponsor_e (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE sponsor_e_event ADD CONSTRAINT FK_ED8F5E671F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
@@ -33,7 +38,12 @@ final class Version20230216164504 extends AbstractMigration
         $this->addSql('ALTER TABLE event DROP FOREIGN KEY FK_3BAE0AA7D936B2FA');
         $this->addSql('ALTER TABLE event_user DROP FOREIGN KEY FK_92589AE271F7E88B');
         $this->addSql('ALTER TABLE event_user DROP FOREIGN KEY FK_92589AE2A76ED395');
+        $this->addSql('ALTER TABLE sponsor_e_event DROP FOREIGN KEY FK_ED8F5E6541E87EF');
+        $this->addSql('ALTER TABLE sponsor_e_event DROP FOREIGN KEY FK_ED8F5E671F7E88B');
         $this->addSql('DROP TABLE event');
         $this->addSql('DROP TABLE event_user');
+        $this->addSql('DROP TABLE sponsor_e');
+        $this->addSql('DROP TABLE sponsor_e_event');
+        $this->addSql('DROP TABLE user');
     }
 }

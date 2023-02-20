b276a926cb147289fe2348171ae4e3666677ec22
louayth <thamilouay@gmail.com>
2023-02-20 19:40:56 +0100

    louay

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -6,12 +6,16 @@ use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 
 class ProduitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            
             ->add('libelle')
             ->add('prix')
             ->add('stock')
